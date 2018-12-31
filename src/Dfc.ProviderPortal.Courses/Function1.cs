1cdd88f6fb478702663a2c52d74f835ce90eaaa1
Mahmood Akhtar <mahmood233@hotmail.com>
2018-12-31 10:55:58 +0000

    Changing the folder structure to suit build pipeline expectations.

--- src/Dfc.ProviderPortal.Courses/Function1.cs
+++ src/Dfc.ProviderPortal.Courses/Function1.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Dfc.ProviderPortal.Courses.Functions
+namespace Dfc.ProviderPortal.Courses
 {
     public static class Function1
     {
