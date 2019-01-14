5b63c6a8a40fbb204b42756c53a434c4376f9f13
Anatoli Malev <makcom@hotmail.co.uk>
2019-01-14 13:21:52 +0000

    modifying course and courserun models

--- src/Dfc.ProviderPortal.Courses/Interfaces/ICourseRun.cs
+++ src/Dfc.ProviderPortal.Courses/Interfaces/ICourseRun.cs
@@ -1,30 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using System;
+using Dfc.ProviderPortal.Courses.Models;
 
 namespace Dfc.ProviderPortal.Courses.Interfaces
 {
     public interface ICourseRun
     {
         Guid id { get; set; }
-        Guid VenueId { get; set; }
-
+        int? CourseInstanceId { get; set; }
+        Guid? VenueId { get; set; }
+       
         string CourseName { get; set; }
         string ProviderCourseID { get; set; }
-        int DeliveryMode { get; set; }
+        DeliveryMode DeliveryMode { get; set; }
         bool FlexibleStartDate { get; set; }
-        DateTime StartDate { get; set; }
+        DateTime? StartDate { get; set; }
         string CourseURL { get; set; }
-        decimal Cost { get; set; }
+        decimal? Cost { get; set; } 
         string CostDescription { get; set; }
-        int DurationUnit { get; set; }
-        int DurationValue { get; set; }
-        int StudyMode { get; set; }
-        int AttendancePattern { get; set; }
+        DurationUnit DurationUnit { get; set; }
+        int? DurationValue { get; set; }
+        StudyMode StudyMode { get; set; } 
+        AttendancePattern AttendancePattern { get; set; } 
 
         DateTime CreatedDate { get; set; }
         string CreatedBy { get; set; }
-        DateTime UpdatedDate { get; set; }
+        DateTime? UpdatedDate { get; set; }
         string UpdatedBy { get; set; }
     }
 }
--- src/Dfc.ProviderPortal.Courses/Models/Course.cs
+++ src/Dfc.ProviderPortal.Courses/Models/Course.cs
@@ -1,21 +1,20 @@
 ﻿using Dfc.ProviderPortal.Courses.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Dfc.ProviderPortal.Courses.Models
 {
-    public class Course : ICourse
+    public class Course : ICourse 
     {
         public Guid id { get; set; }
+        public int? CourseId { get; set; }
+        public string QualificationCourseTitle { get; set; } 
+        public string LearnAimRef { get; set; } 
+        public string NotionalNVQLevelv2 { get; set; } 
+        public string AwardOrgCode { get; set; } 
+        public string QualificationType { get; set; } 
 
-        public string QualificationCourseTitle { get; set; }
-        public string LearnAimRef { get; set; }
-        public string NotionalNVQLevelv2 { get; set; }
-        public string AwardOrgCode { get; set; }
-        public string QualificationType { get; set; }
-
-        public int ProviderUKPRN { get; set; }
+        public int ProviderUKPRN { get; set; } 
 
         public string CourseDescription { get; set; }
         public string EntryRequirments { get; set; }
@@ -26,7 +25,12 @@ namespace Dfc.ProviderPortal.Courses.Models
         public string WhereNext { get; set; }
 
         public bool AdvancedLearnerLoan { get; set; }
-
+      
         public IEnumerable<CourseRun> CourseRuns { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
     }
 }
