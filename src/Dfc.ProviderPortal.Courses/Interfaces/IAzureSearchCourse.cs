8581bae3681095c9aebaf8c29826641a293ecbbe
Ian <44667988+ian-findacourse@users.noreply.github.com>
2019-01-23 09:15:43 +0000

    Dfc 6555 find a couse webapi (#7)
    * wip
    
    * wip
    
    * DFC 6555 - Add FindACourseAzureSearchData function
    
    * Added courses/populatesearch API
    
    * DFC 6555 - Correct types for azure search
    
    * Fix after merge
    
    * Post merge build problems
    
    * Change granularity of Azure search POCO
    
    * Remove commented out code

--- src/Dfc.ProviderPortal.Courses/Interfaces/IAzureSearchCourse.cs
+++ src/Dfc.ProviderPortal.Courses/Interfaces/IAzureSearchCourse.cs
@@ -10,15 +10,16 @@ namespace Dfc.ProviderPortal.Courses.Interfaces
     public interface IAzureSearchCourse
     {
         Guid id { get; set; }
+        Guid CourseId { get; set; }
         string QualificationCourseTitle { get; set; }
         string LearnAimRef { get; set; }
         string NotionalNVQLevelv2 { get; set; }
         DateTime? UpdatedOn { get; set; }
-        string[] VenueName { get; set; }
-        string[] VenueAddress { get; set; }
-        string[] VenueLattitude { get; set; }
-        string[] VenueLongitude { get; set; }
-        string[] VenueAttendancePattern { get; set; }
+        string VenueName { get; set; }
+        string VenueAddress { get; set; }
+        string VenueLattitude { get; set; }
+        string VenueLongitude { get; set; }
+        int VenueAttendancePattern { get; set; }
         string ProviderName { get; set; }
     }
 }
