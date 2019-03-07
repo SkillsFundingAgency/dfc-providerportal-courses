d77d37e20622436d0db2447b861d2085565bce8d
Ian Rose <ian.findacourse@gmail.com>
2019-03-07 17:52:46 +0000

    wip

--- src/Dfc.ProviderPortal.Courses/WebJobsExtensionStartup.cs
+++ src/Dfc.ProviderPortal.Courses/WebJobsExtensionStartup.cs
@@ -41,6 +41,7 @@ namespace Dfc.ProviderPortal.Courses
             builder.Services.AddScoped<ICosmosDbHelper, CosmosDbHelper>();
             builder.Services.AddScoped<ISearchServiceWrapper, SearchServiceWrapper>();
             builder.Services.AddScoped<ICourseService, CoursesService>();
+            builder.Services.AddScoped<ICosmosDbSettings, CosmosDbSettings>();
             builder.Services.AddTransient((provider) => new HttpClient());
         }
     }
