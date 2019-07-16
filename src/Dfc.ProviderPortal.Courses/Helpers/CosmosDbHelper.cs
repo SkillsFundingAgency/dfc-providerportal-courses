82b3c19fcd26a3111ba257bd8ddde7f6b59904c0
Stephen Yates <SYatesSoftware@gmail.com>
2019-07-16 11:52:41 +0100

    cour-1230

--- src/Dfc.ProviderPortal.Courses/Helpers/CosmosDbHelper.cs
+++ src/Dfc.ProviderPortal.Courses/Helpers/CosmosDbHelper.cs
@@ -167,7 +167,7 @@ namespace Dfc.ProviderPortal.Courses.Helpers
             foreach (var doc in docs)
             {
                 Uri docUri = UriFactory.CreateDocumentUri(_settings.DatabaseId, collectionId, doc.id.ToString());
-                var result = await client.DeleteDocumentAsync(docUri);
+                var result = await client.DeleteDocumentAsync(docUri, new RequestOptions() { PartitionKey = new PartitionKey(doc.ProviderUKPRN) });
 
                 if(result.StatusCode == HttpStatusCode.NoContent)
                 {
@@ -202,7 +202,7 @@ namespace Dfc.ProviderPortal.Courses.Helpers
             foreach (var doc in bulkUploadDocs)
             {
                 Uri docUri = UriFactory.CreateDocumentUri(_settings.DatabaseId, collectionId, doc.id.ToString());
-                var result = await client.DeleteDocumentAsync(docUri);
+                var result = await client.DeleteDocumentAsync(docUri, new RequestOptions() { PartitionKey = new PartitionKey(doc.ProviderUKPRN) });
 
                 if (result.StatusCode == HttpStatusCode.NoContent)
                 {
