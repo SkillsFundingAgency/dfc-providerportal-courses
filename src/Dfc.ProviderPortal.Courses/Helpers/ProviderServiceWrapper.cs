90937e6e12db72c2ced9ec4419f096189a308007
James Gunn <james@gunn.io>
2019-12-09 09:01:32 +0000

    Improve performance of CourseDetail operation
    Call dependent services in parallel.

--- src/Dfc.ProviderPortal.Courses/Helpers/ProviderServiceWrapper.cs
+++ src/Dfc.ProviderPortal.Courses/Helpers/ProviderServiceWrapper.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,57 +10,53 @@ using Dfc.ProviderPortal.Courses.Models;
 using Dfc.ProviderPortal.Courses.Settings;
 using Dfc.ProviderPortal.Courses.Interfaces;
 
-
 namespace Dfc.ProviderPortal.Courses.Helpers
 {
     public class ProviderServiceWrapper : IProviderServiceWrapper
     {
         private readonly IProviderServiceSettings _settings;
-        private readonly HttpClient _client;
 
-        public ProviderServiceWrapper(IProviderServiceSettings settings, HttpClient client)
+        public ProviderServiceWrapper(IProviderServiceSettings settings)
         {
             Throw.IfNull(settings, nameof(settings));
             _settings = settings;
-
-            Throw.IfNull(client, nameof(client));
-            _client = client;
         }
 
-        public dynamic GetByPRN(int PRN)
+        public async Task<dynamic> GetByPRN(int PRN)
         {
-            // Call service to get data
-            //HttpClient client = new HttpClient();
-            _client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _settings.ApiKey);
-            //var criteria = new { PRN };
-            //StringContent content = new StringContent(JsonConvert.SerializeObject(criteria), Encoding.UTF8, "application/json");
-            Task<HttpResponseMessage> taskResponse = _client.GetAsync($"{_settings.ApiUrl}GetProviderByPRN?PRN={PRN}"); //, content);
-            taskResponse.Wait();
-            Task<string> taskJSON = taskResponse.Result.Content.ReadAsStringAsync();
-            taskJSON.Wait();
-            string json = taskJSON.Result;
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _settings.ApiKey);
 
-            // Return data as model object
-            return JsonConvert.DeserializeObject<dynamic>(json);
+                var response = await httpClient.GetAsync($"{_settings.ApiUrl}GetProviderByPRN?PRN={PRN}");
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<dynamic>(json);
+            }
         }
 
         public IEnumerable<AzureSearchProviderModel> GetLiveProvidersForAzureSearch()
         {
-            // Call service to get data
-            //HttpClient client = new HttpClient();
-            _client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _settings.ApiKey);
-            var criteria = new object();
-            StringContent content = new StringContent(JsonConvert.SerializeObject(criteria), Encoding.UTF8, "application/json");
-            Task<HttpResponseMessage> taskResponse = _client.PostAsync($"{_settings.ApiUrl}GetLiveProvidersForAzureSearch", content);
-            taskResponse.Wait();
-            Task<string> taskJSON = taskResponse.Result.Content.ReadAsStringAsync();
-            taskJSON.Wait();
-            string json = taskJSON.Result;
-                
-            // Return data as model objects
-            if (!json.StartsWith("["))
-                json = "[" + json + "]";
-            return JsonConvert.DeserializeObject<IEnumerable<AzureSearchProviderModel>>(json);
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _settings.ApiKey);
+
+                // Call service to get data
+                var criteria = new object();
+                StringContent content = new StringContent(JsonConvert.SerializeObject(criteria), Encoding.UTF8, "application/json");
+                Task<HttpResponseMessage> taskResponse = httpClient.PostAsync($"{_settings.ApiUrl}GetLiveProvidersForAzureSearch", content);
+                taskResponse.Wait();
+                Task<string> taskJSON = taskResponse.Result.Content.ReadAsStringAsync();
+                taskJSON.Wait();
+                string json = taskJSON.Result;
+
+                // Return data as model objects
+                if (!json.StartsWith("["))
+                    json = "[" + json + "]";
+                return JsonConvert.DeserializeObject<IEnumerable<AzureSearchProviderModel>>(json);
+            }
         }
     }
 }
