f90451eec9eb50e94a17818ad1222919b3e2708a
James Gunn <james@gunn.io>
2019-12-13 11:50:15 +0000

    Add FeChoice to CourseDetail response

---
+++ src/Dfc.ProviderPortal.Courses/Interfaces/IReferenceDataServiceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfc.ProviderPortal.Courses.Interfaces
+{
+    public interface IReferenceDataServiceSettings
+    {
+        string ApiUrl { get; set; }
+        string ApiKey { get; set; }
+    }
+}
