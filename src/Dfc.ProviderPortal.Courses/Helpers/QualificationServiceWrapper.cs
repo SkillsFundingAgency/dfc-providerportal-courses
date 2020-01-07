004b4e1c2bf47e7d9b18f4a10f1f63efcab72328
James Gunn <james@gunn.io>
2020-01-07 09:26:05 +0000

    Dispose SearchServiceClient

--- src/Dfc.ProviderPortal.Courses/Helpers/QualificationServiceWrapper.cs
+++ src/Dfc.ProviderPortal.Courses/Helpers/QualificationServiceWrapper.cs
@@ -2,30 +2,27 @@
 using Dfc.ProviderPortal.Courses.Interfaces;
 using Dfc.ProviderPortal.Packages;
 using Microsoft.Azure.Search;
-using Microsoft.Azure.Search.Models;
-using Microsoft.Extensions.Logging;
 
 namespace Dfc.ProviderPortal.Courses.Helpers
 {
     public class QualificationServiceWrapper : IQualificationServiceWrapper
     {
-        private readonly ILogger _log;
         private readonly IQualificationServiceSettings _settings;
-        private static SearchServiceClient _service;
-        private static ISearchIndexClient _index;
 
         public QualificationServiceWrapper(IQualificationServiceSettings settings)
         {
             Throw.IfNull(settings, nameof(settings));
             _settings = settings;
-
-            _service = new SearchServiceClient(settings.SearchService, new SearchCredentials(settings.QueryKey));
-            _index = _service.Indexes.GetClient(_settings.Index);
         }
 
-        public Task<dynamic> GetQualificationById(string LARSRef)
+        public async Task<dynamic> GetQualificationById(string LARSRef)
         {
-            return _index.Documents.GetAsync<dynamic>(LARSRef);
+            using (var service = new SearchServiceClient(_settings.SearchService, new SearchCredentials(_settings.QueryKey)))
+            {
+                var index = service.Indexes.GetClient(_settings.Index);
+
+                return await index.Documents.GetAsync<dynamic>(LARSRef);
+            }
         }
     }
 }
